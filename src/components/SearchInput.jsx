edd76ab02ee6bee7014a5f19e4997113bd1c115d
Kevin <kevpadilla01@gmail.com>
2024-10-07 18:11:21 -0300

    ft - SearchInput component added

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
 import { SearchIcon } from "../assets/Icons"
+import useFetch from "../hooks/useFetch"
 
 const SearchInput = () => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const URL = import.meta.env.VITE_URL
+
+  const { data, loading, error } = useFetch(URL, searchQuery)
+
+  console.log(data)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setSearchQuery(e.target.elements.SearchQuery.value)
+  }
+
   return (
-    <div className="relative w-full">
+    <form onSubmit={handleSubmit} className="relative w-full">
       <label htmlFor="SearchQuery" className="sr-only"> Search </label>
 
       <input
         type="text"
+        name="SearchQuery"
         id="SearchQuery"
         placeholder="Enter your keywords..."
         className="text-sm sm:text-base w-full rounded-full border border-gray-200 pe-10 py-3 sm:p-3 dark:border-neutral-900 dark:bg-neutral-900 dark:text-gray-300 focus:ring-0 focus:outline-none focus:border-neutral-500 transition-colors"
@@ -17,7 +32,15 @@ const SearchInput = () => {
       >
         <SearchIcon />
       </span>
-    </div>
+
+      {/* <div className="w-full h-fit">
+        {data ? data.map((photo) => (
+          <article key={photo}>
+
+          </article>
+        ))}
+      </div> */}
+    </form>
   )
 }
 

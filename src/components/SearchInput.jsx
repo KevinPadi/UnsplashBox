812b42dd7c3953f39fd96ad38adf90221938ea80
Kevin <kevpadilla01@gmail.com>
2024-10-13 22:28:29 -0300

    ft - search input updated

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,21 +1,27 @@
-import { useState } from "react"
-import { SearchIcon } from "../assets/Icons"
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { SearchIcon } from "../assets/Icons";
 import { useNavigate } from "react-router-dom";
 
-const SearchInput = () => {
-  const [searchQuery, setSearchQuery] = useState('')
-  const navigate = useNavigate()
+const SearchInput = ({ variant }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (searchQuery.trim() !== '') {
-      navigate(`/results/search/${searchQuery}`)
+    e.preventDefault();
+    if (searchQuery.trim() !== "") {
+      navigate(`/results/search/${searchQuery}`);
     }
-  }
+  };
+
+  const paddingClasses = variant === "home" ? "py-3 sm:py-3" : "p-1"
+  const isVisibleClasses = variant === "hamburguerMenu" ? "hidden md:block" : ""
 
   return (
-    <form onSubmit={handleSubmit} className="relative w-full">
-      <label htmlFor="SearchQuery" className="sr-only"> Search </label>
+    <form onSubmit={handleSubmit} className={`relative w-full ${isVisibleClasses}`}>
+      <label htmlFor="SearchQuery" className="sr-only">
+        Search
+      </label>
 
       <input
         type="text"
@@ -24,16 +30,14 @@ const SearchInput = () => {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
         placeholder="Enter your keywords..."
-        className="text-sm sm:text-base w-full rounded-full border border-gray-200 pe-10 py-3 sm:p-3 dark:border-neutral-900 dark:bg-neutral-900 dark:text-gray-300 focus:ring-0 focus:outline-none focus:border-neutral-500 transition-colors"
+        className={`text-sm sm:text-base w-full rounded-full border border-neutral-800 pe-10 ${paddingClasses} border-neutral-90 bg-neutral-900 text-gray-300 focus:ring-0 focus:outline-none focus:border-neutral-700 transition-colors ps-3`}
       />
 
-      <span
-        className="pointer-events-none absolute inset-y-0 end-0 grid w-14 place-content-center text-gray-500 dark:text-gray-400"
-      >
+      <span className="pointer-events-none absolute inset-y-0 end-0 grid w-14 place-content-center text-gray-500 dark:text-gray-400">
         <SearchIcon />
       </span>
     </form>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;

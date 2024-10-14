6b272e9b65d2542da3536def887d545b2c63896f
Kevin <kevpadilla01@gmail.com>
2024-10-13 22:27:50 -0300

    react router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ResultsPage from './pages/ResultsPage.jsx'
+import Layout from './components/Layout.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -11,11 +12,17 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/results/search/:query",
-    element: <ResultsPage />,
+    element: <Layout />, // Usa el Layout con el Navbar
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/results/search/:query",
+        element: <ResultsPage />,
+      },
+    ],
   },
 ]);
 
